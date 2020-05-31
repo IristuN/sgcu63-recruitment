4ace2e6775765a75c48e018d72655b879e2b208e
IristuN <nutrock007@gmail.com>
2020-05-31 22:50:20 +0700

    Prob3: add comment

--- Prob#3-Numericalization/Prob3.py
+++ Prob#3-Numericalization/Prob3.py
@@ -1,4 +1,4 @@
-#defualt number
+#generate defualt-sized number
 numString = [
         ['0'*5] + ['0'+3*' '+'0']*3 + ['0'*5], #0
         [' '*4+'1']*5, #1
@@ -12,14 +12,24 @@ numString = [
         ['9'*5, '9' + ' '*3 + '9', '9'*5, ' '*4 + '9', '9'*5] #9
 ]
 
+#input
 N, M1, M2 = input().split()
 M1, M2 = int(M1), int(M2)
 
+#generate number for each row
 for row in range(5):
   text = ''
+
+  #add number for each column
   for number in N:
+
+    #add each numbers' row 
     for t in numString[int(number)][row]:
       text += M2*t
+
+    #add space of each number
     text += ' '*M2
+
+  #multiply height of number
   for height in range(M1):
     print(text)
