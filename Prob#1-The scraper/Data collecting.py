80f7cc1a841fead1670310b4fe1378d039c01793
IristuN <nutrock007@gmail.com>
2020-06-01 15:26:45 +0700

    Prob1: add comment

--- Prob#1-The scraper/Data collecting.py
+++ Prob#1-The scraper/Data collecting.py
@@ -2,7 +2,9 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+
 url = "https://rubnongkaomai.com"
+#get request and parse text to json 
 response = requests.get("https://rubnongkaomai.com/pages-manifest-07f52c14a655550e2770.js")
 text = response.text
 start = text.find("pages:")-1
@@ -12,9 +14,12 @@ data = json.loads(jsonText)
 
 baan_datas = {}
 baan_datas["baans"] = []
+
+#get path from data
 for item in data["pages"]:
   path = item["path"]
   if ("/baan/" in path) and (path !="/baan/"):
+    #scraping each data of baan from paths
     baan_request = requests.get(url + path)
     baan_soup = BeautifulSoup(baan_request.content, "html.parser")
     baan_data = baan_soup.find("div", {"class":"baan-info-module--text-wrapper--uuYTz"})
@@ -24,5 +29,6 @@ for item in data["pages"]:
     baan["name"] = baan_name
     baan["slogan"] = baan_slogan
     baan_datas["baans"].append(baan)
+#dump json into .json file
 with open("data_file.json", "w", encoding='utf8') as data_file:
     json.dump(baan_datas, data_file, ensure_ascii=False)
